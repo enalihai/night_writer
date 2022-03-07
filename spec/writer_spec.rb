9b466dbf2a22bc56839560d84506025538c2161f
Luke Pascale <lpluke5@gmail.com>
2022-03-06 19:12:51 -0700

    test: Add test for file_convert to get array of letters from message

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -5,16 +5,24 @@ require 'pry'
 RSpec.describe Writer do
   before :each do
     @writer = Writer.new
+    @stub = File.read('mock_message.txt').delete("\n")
   end
+
   describe '#writer' do
     it 'exists' do
+
       expect(@writer).to be_a(Writer)
     end
 
     it 'has a library of braille and english' do
-      # binding.pry
-      expect(@writer.dictionary["a"]).to eq(["0", ".", ".", ".", ".", "."])
-      expect(@writer.dictionary[" "]).to eq([".", ".", ".", ".", ".", "."])
+
+      expect(@writer.dictionary["a"]).to eq(["0.", "..", ".."])
+      expect(@writer.dictionary[" "]).to eq(["..", "..", ".."])
+    end
+
+    it 'can convert a file to its characters' do
+
+      expect(@writer.file_convert(@stub)).to eq(["a", "c", "e", "b", "d"])
     end
   end
 end
