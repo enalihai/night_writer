0c3e408edf878b913204b0c27e6ba162af8f2062
Luke Pascale <lpluke5@gmail.com>
2022-03-06 20:18:14 -0700

    test: Add test for ability to break up into 3 different elements by lines of braille

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -25,9 +25,9 @@ RSpec.describe Writer do
       expect(@writer.file_convert(@stub)).to eq(["a", "c", "e", "b", "d"])
     end
 
-    it 'can convert the new file to braille' do
+    it 'can convert the new file to braille elements' do
 
-      expect(@writer.braille_convert(@stub)).to eq([["0.", "..", ".."], ["00", "..", ".."], ["0.", ".0", ".."], ["0.", "0.", ".."], ["00", ".0", ".."]])
+      expect(@writer.braille_convert(@stub)).to eq([["0.", "00", "0.", "0.", "00"], ["..", "..", ".0", "0.", ".0"], ["..", "..", "..", "..", ".."]])
     end
   end
 end
