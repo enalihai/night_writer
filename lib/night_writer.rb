f51cc1ea44bb21a30e16ba8a652f432d2233dd4a
Luke Pascale <lpluke5@gmail.com>
2022-03-07 16:36:00 -0700

    feat: Update night_writer.rb to include new method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,5 @@
 require './lib/writer'
+require 'pry'
 
 @writer = Writer.new
 
@@ -7,7 +8,7 @@ message_input = cli.read
 
 cli.close
 
-braille_txt = @writer.file_convert(message_input).delete("\n")
+braille_txt = @writer.file_converter(message_input)
 
 message_output = File.open(ARGV[1], "w")
 message_output.write(braille_txt)
